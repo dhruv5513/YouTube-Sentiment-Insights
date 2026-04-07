94f77776cdfdcc6848acb124e3c1606cdb77d49f
Dhruv Goyal <sonurwr512@gmail.com>
2026-04-08 00:08:53 +0530

    updated

--- flask_api/app.py
+++ flask_api/app.py
@@ -15,13 +15,11 @@ from flask_cors import CORS
 import io
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
-import mlflow
 import numpy as np
 import re
 import pandas as pd
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-from mlflow.tracking import MlflowClient
 import matplotlib.dates as mdates
 import pickle
 
