d361e263c734028c14f5f72826c7765d4916376d
Dhruv Goyal <sonurwr512@gmail.com>
2026-04-08 01:22:09 +0530

    updated

--- flask_api/app.py
+++ flask_api/app.py
@@ -346,6 +346,14 @@ def get_comments():
     response = requests.get(url)
     return jsonify(response.json())
 
+@app.route('/')
+def home():
+    return "working"
+
+@app.route('/test')
+def test():
+    return "test working"
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
